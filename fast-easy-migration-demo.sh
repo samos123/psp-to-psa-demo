87ae5acaf907875c67032ae020969d8a73191f43
Sam Stoelinga <sammiestoel@gmail.com>
2022-09-29 14:10:11 -0700

    Initial commit

---
+++ fast-easy-migration-demo.sh
@@ -0,0 +1,57 @@
+#!/usr/bin/env bash
+########################
+# include the magic
+########################
+. demo-magic.sh
+
+# hide the evidence
+clear
+
+wait_ready() {
+  kubectl wait --for=condition=Ready $1 > /dev/null
+}
+
+wait_available() {
+  kubectl wait --for=condition=Available $1 > /dev/null
+}
+
+
+# Demonstrate PSP is currently active
+pe "kubectl get psp my-psp -o yaml"
+pe "kubectl get clusterrole my-psp -o yaml"
+pe "kubectl get clusterrolebinding psp-all-sa  -o yaml"
+pe "cat nginx-priv.yaml"
+pe "kubectl apply -f nginx-priv.yaml"
+pe "kubectl describe deployment/nginx-priv"
+pe "kubectl get events --field-selector reason=FailedCreate"
+pe "kubectl delete -f nginx-priv.yaml"
+pe "cat nginx.yaml"
+pe "kubectl apply -f nginx.yaml"
+wait_available deployment/nginx-nonpriv
+pe "kubectl describe deployment/nginx-nonpriv"
+
+# see which pod security standard would work for existing pods
+pe "kubectl label --dry-run=server --overwrite ns default pod-security.kubernetes.io/enforce=restricted"
+pe "kubectl label --dry-run=server --overwrite ns default pod-security.kubernetes.io/enforce=baseline"
+pe "kubectl label --overwrite ns default pod-security.kubernetes.io/enforce=baseline"
+
+# disable psp on namespace default
+pe "cat privileged-psp.yaml"
+pe "kubectl apply -f privileged-psp.yaml"
+pe "kubectl create clusterrole privileged-psp --verb use --resource podsecuritypolicies.policy --resource-name privileged"
+pe "kubectl create -n default rolebinding disable-psp --clusterrole privileged-psp --group system:serviceaccounts:default"
+
+# should see error like this now since PSP is disabled and PSA is active
+# 2s Warning FailedCreate  replicaset/nginx-priv-8c9cd58b5         (combined from similar events): Error creating: pods "nginx-priv-8c9cd58b5-pxhxr" is forbidden: violates PodSecurity "baseline:latest": non-default capabilities (container "nginx" must not include "NET_ADMIN" in securityContext.capabilities.add), privileged (container "nginx" must not set securityContext.privileged=true)
+pe "cat nginx-priv.yaml"
+pe "kubectl apply -f nginx-priv.yaml"
+pe "kubectl describe deployment/nginx-priv"
+pe "kubectl get events --field-selector reason=FailedCreate --sort-by='.metadata.creationTimestamp' | tail -n 1"
+
+# cleanup
+kubectl delete -f nginx.yaml > /dev/null
+kubectl delete -f nginx-priv.yaml > /dev/null
+kubectl delete -f privileged-psp.yaml > /dev/null
+kubectl delete -n default rolebinding disable-psp > /dev/null
+kubectl delete clusterrole privileged-psp > /dev/null
+kubectl label namespaces default pod-security.kubernetes.io/enforce- > /dev/null
